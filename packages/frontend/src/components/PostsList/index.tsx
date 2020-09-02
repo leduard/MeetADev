2c0a25df28f42d1d5941441e36d7dd307360019f
leduard <luizeduardo.patos@gmail.com>
2020-09-02 19:34:45 -0300

    Move posts list logic to a separate component

---
+++ packages/frontend/src/components/PostsList/index.tsx
@@ -0,0 +1,119 @@
+import React, { useState, useCallback, useEffect } from 'react';
+
+import { Container } from './styles';
+
+import Post from '../Post';
+import Spinner from '../Spinner';
+
+import api from '../../services/api';
+import { useAuth } from '../../context/Auth';
+
+interface PostInterface {
+  id: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+  user: {
+    id: string;
+    name: string;
+    username: string;
+    followers_count: number;
+    following_count: number;
+  };
+}
+
+interface PostsListComponent {
+  contentRef: React.RefObject<HTMLDivElement>;
+}
+
+const PostsList: React.FC<PostsListComponent> = ({ contentRef }) => {
+  const [loading, setLoading] = useState(false);
+  const [fetching, setFetching] = useState(false);
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+  const [postsCount, setPostsCount] = useState(0);
+
+  const {
+    user: { token: authToken },
+  } = useAuth();
+
+  const getPosts = useCallback(
+    async (page = 1, isFetch = false) => {
+      if (fetching) return;
+
+      setLoading(!isFetch);
+
+      const { status, data } = await api.get(`/posts?page=${page}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      if (status === 200) {
+        setCurrentPage(page + 1);
+        if (!itemsPerPage) setItemsPerPage(data.itemsPerPage);
+        setPostsCount(data.postsCount);
+        setPosts((prev) => (isFetch ? [...prev, ...data.posts] : data.posts));
+      }
+
+      setLoading(false);
+      setFetching(false);
+    },
+    [authToken, currentPage, fetching], // eslint-disable-line
+  );
+
+  useEffect(() => {
+    getPosts();
+  }, []); // eslint-disable-line
+
+  useEffect(() => {
+    if (!fetching)
+      window.onscroll = async () => {
+        if (contentRef.current) {
+          const { scrollY, innerHeight } = window;
+          const { scrollHeight } = contentRef.current;
+
+          if (scrollY === scrollHeight - innerHeight + 80 && !fetching) {
+            if (
+              !!posts.length &&
+              !(Math.ceil(postsCount / itemsPerPage) >= currentPage)
+            )
+              return;
+            setFetching(true);
+            await getPosts(currentPage, true);
+          }
+        }
+      };
+    else window.onscroll = null;
+  }, [
+    getPosts,
+    currentPage,
+    fetching,
+    posts.length,
+    postsCount,
+    itemsPerPage,
+    contentRef,
+  ]);
+
+  return loading ? (
+    <div style={{ marginTop: '45%' }}>
+      <Spinner color="#edebeb" size={85} width={4} />
+    </div>
+  ) : (
+    <>
+      <Container>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </Container>
+      {fetching && (
+        <div style={{ marginTop: 10, marginBottom: 10 }}>
+          <Spinner color="#edebeb" size={45} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default PostsList;
--- packages/frontend/src/pages/Home/index.tsx
+++ packages/frontend/src/pages/Home/index.tsx
@@ -1,98 +1,12 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useRef } from 'react';
 
-import {
-  Container,
-  App,
-  Content,
-  PostForm,
-  PostsContainer,
-  Image,
-} from './styles';
+import { Container, App, Content, PostForm, Image } from './styles';
 
 import Header from '../../components/Header';
-import Post from '../../components/Post';
-import Spinner from '../../components/Spinner';
-
-import api from '../../services/api';
-import { useAuth } from '../../context/Auth';
-
-interface PostInterface {
-  id: string;
-  content: string;
-  created_at: Date;
-  updated_at: Date;
-  user: {
-    id: string;
-    name: string;
-    username: string;
-    followers_count: number;
-    following_count: number;
-  };
-}
+import PostsList from '../../components/PostsList';
 
 const Home: React.FC = () => {
-  const [loading, setLoading] = useState(false);
-  const [fetching, setFetching] = useState(false);
-  const [posts, setPosts] = useState<PostInterface[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(0);
-  const [postsCount, setPostsCount] = useState(0);
-
   const ContentRef = useRef<HTMLDivElement>(null);
-
-  const {
-    user: { token: authToken },
-  } = useAuth();
-
-  const getPosts = useCallback(
-    async (page = 1, isFetch = false) => {
-      if (fetching) return;
-
-      setLoading(!isFetch);
-
-      const { status, data } = await api.get(`/posts?page=${page}`, {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-        },
-      });
-
-      if (status === 200) {
-        setCurrentPage(page + 1);
-        if (!itemsPerPage) setItemsPerPage(data.itemsPerPage);
-        setPostsCount(data.postsCount);
-        setPosts((prev) => (isFetch ? [...prev, ...data.posts] : data.posts));
-      }
-
-      setLoading(false);
-      setFetching(false);
-    },
-    [authToken, currentPage, fetching], // eslint-disable-line
-  );
-
-  useEffect(() => {
-    getPosts();
-  }, []); // eslint-disable-line
-
-  useEffect(() => {
-    if (!fetching)
-      window.onscroll = async () => {
-        if (ContentRef.current) {
-          const { scrollY, innerHeight } = window;
-          const { scrollHeight } = ContentRef.current;
-
-          if (scrollY === scrollHeight - innerHeight + 80 && !fetching) {
-            if (
-              !!posts.length &&
-              !(Math.ceil(postsCount / itemsPerPage) >= currentPage)
-            )
-              return;
-            setFetching(true);
-            await getPosts(currentPage, true);
-          }
-        }
-      };
-    else window.onscroll = null;
-  }, [getPosts, currentPage, fetching, posts.length, postsCount, itemsPerPage]);
 
   return (
     <Container>
@@ -104,25 +18,7 @@ const Home: React.FC = () => {
             <textarea placeholder="o que você descobriu hoje?" />
             <button type="button">postar</button>
           </PostForm>
-
-          {loading ? (
-            <div style={{ marginTop: '45%' }}>
-              <Spinner color="#edebeb" size={85} width={4} />
-            </div>
-          ) : (
-            <>
-              <PostsContainer>
-                {posts.map((post) => (
-                  <Post key={post.id} post={post} />
-                ))}
-              </PostsContainer>
-              {fetching && (
-                <div style={{ marginTop: 10, marginBottom: 10 }}>
-                  <Spinner color="#edebeb" size={45} />
-                </div>
-              )}
-            </>
-          )}
+          <PostsList contentRef={ContentRef} />
         </Content>
       </App>
       <Image />
