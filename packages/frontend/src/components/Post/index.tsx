f5f4c586b8d78c0cf5801d9f36a568f0a714e1f1
leduard <luizeduardo.patos@gmail.com>
2020-09-02 19:16:11 -0300

    Home page getting data from api

--- packages/frontend/src/components/Post/index.tsx
+++ packages/frontend/src/components/Post/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { FiHeart } from 'react-icons/fi';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 import {
   Container,
@@ -10,46 +11,61 @@ import {
   Content,
 } from './styles';
 
-const Post: React.FC = () => {
+interface PostInterface {
+  id: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+  user: {
+    id: string;
+    name: string;
+    username: string;
+    followers_count: number;
+    following_count: number;
+  };
+}
+
+interface PostComponent {
+  post: PostInterface;
+}
+
+const Post: React.FC<PostComponent> = ({ post }) => {
   const history = useHistory();
 
   return (
     <Container
       onClick={(e): void => {
         e.stopPropagation();
-        history.push('/leduardo/s/post-uuid');
+        history.push(`/${post.user.username}/s/${post.id}`);
       }}
     >
       <ProfileImage
         onClick={(e): void => {
           e.stopPropagation();
-          history.push('/leduardo');
+          history.push(`/${post.user.username}`);
         }}
       >
-        L
+        {post.user.username.charAt(0)}
       </ProfileImage>
       <ContentWrapper>
         <Header>
-          <h5 style={{ fontSize: 14 }}>@leduardo</h5>
-          <h5 style={{ fontSize: 12 }}>50min atrás</h5>
+          <h5 style={{ fontSize: 14 }}>{`@${post.user.username}`}</h5>
+          <h5 style={{ fontSize: 12 }}>
+            {`${formatDistanceToNow(new Date(post.created_at), {
+              locale: ptBR,
+              includeSeconds: true,
+            })} atrás`}
+          </h5>
         </Header>
-        <Content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Content>
-        <FiHeart
+        <Content>{post.content}</Content>
+        {/* <FiHeart
           onClick={(e): void => {
             e.stopPropagation();
             console.log('like');
           }}
           size={25}
           strokeWidth={1}
-        />
+        /> */}
       </ContentWrapper>
     </Container>
   );
--- packages/frontend/src/pages/Home/index.tsx
+++ packages/frontend/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 
 import {
   Container,
@@ -11,24 +11,118 @@ import {
 
 import Header from '../../components/Header';
 import Post from '../../components/Post';
+import Spinner from '../../components/Spinner';
+
+import api from '../../services/api';
+import { useAuth } from '../../context/Auth';
+
+interface PostInterface {
+  id: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+  user: {
+    id: string;
+    name: string;
+    username: string;
+    followers_count: number;
+    following_count: number;
+  };
+}
 
 const Home: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [fetching, setFetching] = useState(false);
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+  const [postsCount, setPostsCount] = useState(0);
+
+  const ContentRef = useRef<HTMLDivElement>(null);
+
+  const {
+    user: { token: authToken },
+  } = useAuth();
+
+  const getPosts = useCallback(
+    async (page = 1, isFetch = false) => {
+      if (fetching) return;
+
+      setLoading(!isFetch);
+
+      const { status, data } = await api.get(`/posts?page=${page}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      if (status === 200) {
+        setCurrentPage(page + 1);
+        if (!itemsPerPage) setItemsPerPage(data.itemsPerPage);
+        setPostsCount(data.postsCount);
+        setPosts((prev) => (isFetch ? [...prev, ...data.posts] : data.posts));
+      }
+
+      setLoading(false);
+      setFetching(false);
+    },
+    [authToken, currentPage, fetching], // eslint-disable-line
+  );
+
+  useEffect(() => {
+    getPosts();
+  }, []); // eslint-disable-line
+
+  useEffect(() => {
+    if (!fetching)
+      window.onscroll = async () => {
+        if (ContentRef.current) {
+          const { scrollY, innerHeight } = window;
+          const { scrollHeight } = ContentRef.current;
+
+          if (scrollY === scrollHeight - innerHeight + 80 && !fetching) {
+            if (
+              !!posts.length &&
+              !(Math.ceil(postsCount / itemsPerPage) >= currentPage)
+            )
+              return;
+            setFetching(true);
+            await getPosts(currentPage, true);
+          }
+        }
+      };
+    else window.onscroll = null;
+  }, [getPosts, currentPage, fetching, posts.length, postsCount, itemsPerPage]);
+
   return (
     <Container>
       <Image />
       <App>
         <Header />
-        <Content>
+        <Content ref={ContentRef}>
           <PostForm>
             <textarea placeholder="o que você descobriu hoje?" />
             <button type="button">postar</button>
           </PostForm>
 
-          <PostsContainer>
-            {['', '', ''].map((post) => (
-              <Post key={Math.random()} />
-            ))}
-          </PostsContainer>
+          {loading ? (
+            <div style={{ marginTop: '45%' }}>
+              <Spinner color="#edebeb" size={85} width={4} />
+            </div>
+          ) : (
+            <>
+              <PostsContainer>
+                {posts.map((post) => (
+                  <Post key={post.id} post={post} />
+                ))}
+              </PostsContainer>
+              {fetching && (
+                <div style={{ marginTop: 10, marginBottom: 10 }}>
+                  <Spinner color="#edebeb" size={45} />
+                </div>
+              )}
+            </>
+          )}
         </Content>
       </App>
       <Image />
